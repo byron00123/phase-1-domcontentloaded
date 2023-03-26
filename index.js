ab4e7aa7c7449929d3f67707047d5f0f6553d9a8
Byron Omondi <omondibyron25@gmail.com>
2023-03-26 11:49:36 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+  // target the paragraph element with id "text"
+  const paragraph = document.getElementById("text");
+  
+  // replace the text of the paragraph element with "This is really cool!"
+  paragraph.textContent = "This is really cool!";
+});
